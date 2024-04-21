6d66605bce9de0e292a725678e7313d067a185bc
ArturKlasa <klasaartur@gmail.com>
2024-04-21 17:55:03 -0500

    Added supabase

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = "https://yzdkxsrymnrvqxbvxwct.supabase.co";
+  static const supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6ZGt4c3J5bW5ydnF4YnZ4d2N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM3Mzk1MDAsImV4cCI6MjAyOTMxNTUwMH0.XG8KNCHRPgsFgQP9ZqagtgWRun6hcwEMX9MgdwlSVSA";
+}
